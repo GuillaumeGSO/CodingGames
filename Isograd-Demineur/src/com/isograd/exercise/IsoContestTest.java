fc59c0dd2cafe2d043ae0cdf8793e1829be70381
Guillaume SOURIS <gsouris@generali.fr>
2018-01-30 16:44:42 +0100

    Démineur : refactoring des TU

--- Isograd-Demineur/src/com/isograd/exercise/IsoContestTest.java
+++ Isograd-Demineur/src/com/isograd/exercise/IsoContestTest.java
@@ -1,5 +1,7 @@
 package com.isograd.exercise;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
@@ -14,125 +16,41 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class IsoContestTest {
 
+
     @Test
     public void testInput1() throws Exception {
-        Path pathInput = Paths.get("input1.txt");
-        Path pathOutput = Paths.get("output1.txt");
-
-        byte[] data = Files.readAllBytes(pathInput);
-
-        ByteArrayInputStream in = new ByteArrayInputStream(data);
-        ByteArrayOutputStream  out = new ByteArrayOutputStream();
-        System.setIn(in);
-        System.setOut(new PrintStream(out));
-
-        // do your thing
-        IsoContest.main(null);
-
-        String reponseAttendue = Files.readAllLines(pathOutput).get(0);
-        assertEquals(reponseAttendue, out.toString().replaceAll("\\r\\n", ""));
-
-        // optionally, reset System.in to its original
-        System.setIn(System.in);
-        System.setOut(System.out);
+        runTest("1");
     }
 
     @Test
     public void testInput2() throws Exception {
-        Path pathInput = Paths.get("input2.txt");
-        Path pathOutput = Paths.get("output2.txt");
-
-        byte[] data = Files.readAllBytes(pathInput);
-
-        ByteArrayInputStream in = new ByteArrayInputStream(data);
-        ByteArrayOutputStream  out = new ByteArrayOutputStream();
-        System.setIn(in);
-        System.setOut(new PrintStream(out));
-
-        // do your thing
-        IsoContest.main(null);
-
-        String reponseAttendue = Files.readAllLines(pathOutput).get(0);
-        assertEquals(reponseAttendue, out.toString().replaceAll("\\r\\n", ""));
-
-        // optionally, reset System.in to its original
-        System.setIn(System.in);
-        System.setOut(System.out);
+        runTest("2");
     }
 
     @Test
     public void testInput3() throws Exception {
-        Path pathInput = Paths.get("input3.txt");
-        Path pathOutput = Paths.get("output3.txt");
-
-        byte[] data = Files.readAllBytes(pathInput);
-
-        ByteArrayInputStream in = new ByteArrayInputStream(data);
-        ByteArrayOutputStream  out = new ByteArrayOutputStream();
-        System.setIn(in);
-        System.setOut(new PrintStream(out));
-
-        // do your thing
-        IsoContest.main(null);
-
-        String reponseAttendue = Files.readAllLines(pathOutput).get(0);
-        assertEquals(reponseAttendue, out.toString().replaceAll("\\r\\n", ""));
-
-        // optionally, reset System.in to its original
-        System.setIn(System.in);
-        System.setOut(System.out);
+        runTest("3");
     }
 
     @Test
     public void testInput4() throws Exception {
-        Path pathInput = Paths.get("input4.txt");
-        Path pathOutput = Paths.get("output4.txt");
-
-        byte[] data = Files.readAllBytes(pathInput);
-
-        ByteArrayInputStream in = new ByteArrayInputStream(data);
-        ByteArrayOutputStream  out = new ByteArrayOutputStream();
-        System.setIn(in);
-        System.setOut(new PrintStream(out));
-
-        // do your thing
-        IsoContest.main(null);
-
-        String reponseAttendue = Files.readAllLines(pathOutput).get(0);
-        assertEquals(reponseAttendue, out.toString().replaceAll("\\r\\n", ""));
-
-        // optionally, reset System.in to its original
-        System.setIn(System.in);
-        System.setOut(System.out);
+        runTest("4");
     }
 
     @Test
     public void testInput5() throws Exception {
-        Path pathInput = Paths.get("input5.txt");
-        Path pathOutput = Paths.get("output5.txt");
-
-        byte[] data = Files.readAllBytes(pathInput);
-
-        ByteArrayInputStream in = new ByteArrayInputStream(data);
-        ByteArrayOutputStream  out = new ByteArrayOutputStream();
-        System.setIn(in);
-        System.setOut(new PrintStream(out));
-
-        // do your thing
-        IsoContest.main(null);
-
-        String reponseAttendue = Files.readAllLines(pathOutput).get(0);
-        assertEquals(reponseAttendue, out.toString().replaceAll("\\r\\n", ""));
-
-        // optionally, reset System.in to its original
-        System.setIn(System.in);
-        System.setOut(System.out);
+        runTest("5");
     }
 
     @Test
     public void testInput6() throws Exception {
-        Path pathInput = Paths.get("input6.txt");
-        Path pathOutput = Paths.get("output6.txt");
+        runTest("6");
+    }
+
+    private void runTest(String num) throws Exception{
+
+        Path pathInput = Paths.get("input" + num + ".txt");
+        Path pathOutput = Paths.get("output" + num + ".txt");
 
         byte[] data = Files.readAllBytes(pathInput);
 
