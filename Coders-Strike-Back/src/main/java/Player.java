1b06889912a2e9968f4fa6173da6974cd8871c15
GuillaumeGSO <GuillaumeGSO@users.noreply.github.com>
2019-12-01 12:46:25 +0100

    Stabilisation calcul des angles

--- Coders-Strike-Back/src/main/java/Player.java
+++ Coders-Strike-Back/src/main/java/Player.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Map.Entry;
 import java.io.*;
 import java.math.*;
 
@@ -8,157 +9,234 @@ import java.math.*;
  **/
 class Player {
 
-    public static void main(String args[]) {
-        Scanner in = new Scanner(System.in);
+    public static void main(final String args[]) {
+        final Scanner in = new Scanner(System.in);
 
         int power = 100;
-        int seuilDistanceFreinage = 1400;
-        int seuilDistanceBoost = 3 * seuilDistanceFreinage;
-        int seuilBigAngle = 45;
-        int seuilSmallAngle = 20;
-        int nbBoost = 2;
-        int decrement = 20;
-        int increment = 20;
-        int seuil = 10;
-        int nbTours = 0;
-        int nbBases = 0;
+        final int seuilDistanceFreinage = 3000;
+        final int seuilDistanceBoost = 3 * seuilDistanceFreinage;
+        final int seuilBigAngle = 120;
+        final int seuilSmallAngle = 45;
+        final int nbBoost = 2;
+        final int decrement = 5;
+        final int increment = 5;
+        final int seuil = 30;
+        int numeroBase = 1;
 
         // Coordonnees / distance
-        Map<String, Coord> map = new HashMap<>();
-        List<Coord> coords = new ArrayList<>();
+        final Map<Integer, Target> mapOfTargets = new HashMap<>();
+        final Set<String> setOfCoords = new HashSet<>();
 
-        boolean premierTour = true;
         // game loop
         while (true) {
+            // Real position
+            final int x = in.nextInt();
+            final int y = in.nextInt();
 
-            int x = in.nextInt();
-            int y = in.nextInt();
-            int nextCheckpointX = in.nextInt(); // x position of the next check point
-            int nextCheckpointY = in.nextInt(); // y position of the next check point
-            int nextCheckpointDist = in.nextInt(); // distance to the next checkpoint
-            int nextCheckpointAngle = in.nextInt(); // angle between your pod orientation and the direction of the next
-                                                    // checkpoint
-            int opponentX = in.nextInt();
-            int opponentY = in.nextInt();
-            Coord coord = new Coord(nextCheckpointX, nextCheckpointY);
-            String coordStr = coord.toString();
+            // checkpoint
+            final int nextCheckpointX = in.nextInt(); // x position of the next check point
+            final int nextCheckpointY = in.nextInt(); // y position of the next check point
+            final int nextCheckpointDist = in.nextInt(); // distance to the next checkpoint
+            final int nextCheckpointAngle = Math.abs(in.nextInt()); // angle between your pod orientation and the
+                                                                    // direction of the
+            // next
+            // Enemy
+            final int opponentX = in.nextInt();
+            final int opponentY = in.nextInt();
 
-            // Construction de la map des points / distances
-            if (map.get(coordStr) == null) {
-                map.put(coordStr, coord);
-                coords.add(coord);
-                nbBases++;
-                System.err.println("nouvelle : " + coord);
-                //FIXME utliser coord.equals...
+            // Targets
+            final Target nextTarget = new Target(numeroBase, nextCheckpointX, nextCheckpointY);
+            final String nextCoord = nextTarget.toCoord();
+
+            // Construction de la map des points / distances : a chaque detection d'une
+            // nouvelle prochaine base
+            if (!setOfCoords.contains(nextCoord)) {
+                mapOfTargets.put(numeroBase, nextTarget);
+                // On n'initialise la distance qu'à la création
+                nextTarget.distTo = nextCheckpointDist;
+
+                nextTarget.distNext = 1000;
+
+                setOfCoords.add(nextTarget.toCoord());
+
+                // System.err.println("nouvelle : " + nextTarget);
+
+                if (mapOfTargets.get(numeroBase - 1) != null) {
+                    mapOfTargets.get(numeroBase - 1).distNext = mapOfTargets.get(numeroBase).distTo;
+                }
+                numeroBase++;
             }
-            // A la fin du premier tour, on connait toutes les positions et distances
-            // pour améliorer la trajectoire pour les tours suivants
-            if (coords.size() >= 3) {
-                System.err.println(calculAngle(coords.get(1), coords.get(0), coords.get(2)));
-            }    
 
+            int numeroProchaineCible = calculRangProchaineCible(nextCoord, mapOfTargets);
+            Target prochaineCible = mapOfTargets.get(numeroProchaineCible);
 
-            if (premierTour) {
-                // Freinage : quand on arrive proche du prochain point ou que l'angle devient
-                // trop grand
-                if (nextCheckpointDist <= seuilDistanceFreinage || Math.abs(nextCheckpointAngle) >= seuilBigAngle) {
-                    if (power - decrement >= seuil) {
-                        power -= decrement;
-                    }
-                    choose(nextCheckpointX, nextCheckpointY, power,
-                            "down" + " " + nextCheckpointDist + " " + nextCheckpointAngle);
-                    continue;
+            // TODO : ne plus faire ça si tout est trouvé allComplete
+            if (numeroProchaineCible > 0) {
+
+                // Calcul de l'angle entre la source, la cible et la suivante
+                int index = numeroProchaineCible;
+                int indexAvant = index - 1;
+                int indexApres = index + 1;
+
+                if (numeroProchaineCible == 1 && mapOfTargets.size() > 1) {
+                    // System.err.println("avant: " + indexAvant);
+                    indexAvant = mapOfTargets.size();
+                    // System.err.println("avant: " + indexAvant);
+                }
+                if (numeroProchaineCible > 2 && indexApres > mapOfTargets.size()) {
+                    // System.err.println("apres: " + indexApres);
+                    indexApres = 1;
+                    // System.err.println("apres: " + indexApres);
                 }
 
-                // Accéleration : lorsqu'on est loin du prochain point et que l'angle est faible
-                if (nextCheckpointDist > seuilDistanceFreinage && Math.abs(nextCheckpointAngle) <= seuilBigAngle) {
+                // System.err.println("avant: " + indexAvant + " " +
+                // mapOfTargets.get(indexAvant));
+                // System.err.println("index: " + index + " " + mapOfTargets.get(index));
+                // System.err.println("apres: " + indexApres + " " +
+                // mapOfTargets.get(indexApres));
+                if (mapOfTargets.get(indexAvant) != null && mapOfTargets.get(indexApres) != null) {
+                    mapOfTargets.get(indexAvant).angleNext = calculAngle(mapOfTargets.get(indexAvant), prochaineCible,
+                            mapOfTargets.get(indexApres));
+                }
+                // System.err.println("Cible: " + prochaineCible);
+            }
+
+            // Freinage : quand on arrive proche du prochain point ou que l'angle devient
+            // trop grand
+            if (nextCheckpointDist <= seuilDistanceFreinage) {
+                // Premier freinage si à l'approche mais pas trop
+                if (power - decrement >= 2 * seuil) {
+                    power -= decrement;
+                    System.err.println("Freinage approche " + power);
+                    // Deuxième freinage si proche et prochain angle serré
+                    if (prochaineCible.angleNext != null && Math.abs(prochaineCible.angleNext) < seuilBigAngle) {
+                        if (power - 2 * decrement >= seuil) {
+                            power -= 2 * decrement;
+                            System.err.println("Freinage angle serré " + power);
+                        }
+                    }
+                    // troisième freinage si proche et prochaine distance courte
+                    if (prochaineCible.distNext < 2 * seuilDistanceFreinage) {
+                        if (power - decrement >= seuil) {
+                            power -= decrement;
+                            System.err.println("Freinage prochaine distance courte " + power);
+                        }
+                    }
+                }
+                play(nextCheckpointX, nextCheckpointY, power,
+                        "down" + " " + power + " vers: " + prochaineCible.name + "/" + prochaineCible.angleNext);
+                continue;
+            }
+            // Accélération : lorsqu'on est loin du prochain point
+            if (nextCheckpointDist > seuilDistanceFreinage && nextCheckpointAngle < seuilSmallAngle) {
+                if (power + increment <= 100) {
+                    power += increment;
+                    System.err.println("Accélération loin " + power + " " + nextCheckpointAngle);
+                }
+                // Deuxième accélération prochain angle large
+                if (prochaineCible.angleNext != null && Math.abs(prochaineCible.angleNext) > seuilBigAngle) {
                     if (power + increment <= 100) {
                         power += increment;
+                        System.err.println("Accélération grand angle" + power);
                     }
-                    choose(nextCheckpointX, nextCheckpointY, power,
-                            "up" + " " + nextCheckpointDist + " " + nextCheckpointAngle);
-                    continue;
                 }
-                power = 100;
-                choose(nextCheckpointX, nextCheckpointY, power, "max");
-
-            } else {
-                power = 100;
-                System.err.println(map);
-                choose(nextCheckpointX, nextCheckpointY, power, "Fin du premier tour !");
+                // troisième accélération si proche et prochaine distance courte
+                if (prochaineCible.distNext > 2 * seuilDistanceFreinage) {
+                    if (power + increment <= 100) {
+                        power += increment;
+                        System.err.println("Accélération prochaine distance longue " + power);
+                    }
+                }
+                play(nextCheckpointX, nextCheckpointY, power,
+                        "up" + " " + power + " vers: " + prochaineCible.name + "/" + prochaineCible.angleNext);
+                continue;
             }
+
+            // power = 100;
+            play(nextCheckpointX, nextCheckpointY, power,
+                    String.valueOf(power) + " vers: " + prochaineCible.name + "/" + prochaineCible.angleNext);
+
         }
     }
 
-    public static void choose(int nextCheckpointX, int nextCheckpointY, int power, String leReste) {
+    private static int calculRangProchaineCible(String nextCoord, Map<Integer, Target> mapOfTargets) {
+        if (mapOfTargets != null) {
+            for (Integer key : mapOfTargets.keySet()) {
+                if (nextCoord.equals(mapOfTargets.get(key).toCoord())) {
+                    return key;
+                }
+            }
+        }
+        return 0;
+    }
+
+    public static void play(final int nextCheckpointX, final int nextCheckpointY, final int power,
+            final String leReste) {
         System.out.println((int) (nextCheckpointX) + " " + (int) (nextCheckpointY) + " " + (int) power + " " + leReste);
     }
 
-    public static Double calculAngle(Coord pointA, Coord pointB, Coord pointC) {
-        Double distAB = calculDistance(pointA, pointB);
-        Double distAC = calculDistance(pointA, pointC);
-        return Math.toDegrees(Math.acos(calculProduitScalaire(pointA, pointB, pointC)/(distAB * distAC)));
+    public static int calculAngle(final Target pointA, final Target pointB, final Target pointC) {
+
+        // System.err.println("CalculAngle:" + pointA);
+        // System.err.println("CalculAngle:" + pointB);
+        // System.err.println("CalculAngle:" + pointC);
+        final Double distAB = calculDistance(pointA, pointB);
+        final Double distAC = calculDistance(pointA, pointC);
+        Double result = Math.toDegrees(Math.acos(calculProduitScalaire(pointA, pointB, pointC) / (distAB * distAC)));
+        // System.err.println("Angle:" + result);
+        return result.intValue();
     }
 
-    public static Double calculDistance(Coord point1, Coord point2) {
-        Double distX = Double.valueOf(point2.x - point1.x);
-        Double distY = Double.valueOf(point2.y - point1.y);
-        return Math.sqrt(distX*distX + distY*distY);
+    public static Double calculDistance(final Target point1, final Target point2) {
+        final Double distX = Double.valueOf(point2.x - point1.x);
+        final Double distY = Double.valueOf(point2.y - point1.y);
+        return Math.sqrt(distX * distX + distY * distY);
     }
 
     /**
      * Calcul du produit scalaire AB.AC
+     * 
      * @param pointA
      * @param pointB
      * @param pointC
      * @return
      */
-    public static Double calculProduitScalaire(Coord pointA, Coord pointB, Coord pointC) {
-        Double distAB_X = Double.valueOf(pointB.x - pointA.x);
-        Double distAC_X = Double.valueOf(pointC.x - pointA.x);
-        
-        Double distAB_Y = Double.valueOf(pointB.y - pointA.y);
-        Double distAC_Y = Double.valueOf(pointC.y - pointA.y);
+    public static Double calculProduitScalaire(final Target pointA, final Target pointB, final Target pointC) {
+        final Double distAB_X = Double.valueOf(pointB.x - pointA.x);
+        final Double distAC_X = Double.valueOf(pointC.x - pointA.x);
 
-        return (distAB_X*distAC_X) + (distAB_Y*distAC_Y);
+        final Double distAB_Y = Double.valueOf(pointB.y - pointA.y);
+        final Double distAC_Y = Double.valueOf(pointC.y - pointA.y);
+
+        return (distAB_X * distAC_X) + (distAB_Y * distAC_Y);
     }
 }
 
-class Coord {
+class Target {
+    public int name;
     public int x;
     public int y;
+    public int distTo;
+    public int distNext;
+    public Integer angleNext;
 
-    public Coord(int x, int y) {
+    public Target(final int name, final int x, final int y) {
+        this.name = name;
         this.x = x;
         this.y = y;
     }
 
-    public String toString() {
+    public String toCoord() {
         return this.x + "-" + this.y;
     }
 
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + x;
-        result = prime * result + y;
-        return result;
+    public String toString() {
+        return "Name: " + name + " x:" + this.x + " y:" + this.y + " to:" + this.distTo + " next:" + this.distNext
+                + " angle: " + angleNext;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Coord other = (Coord) obj;
-        if (x != other.x)
-            return false;
-        if (y != other.y)
-            return false;
-        return true;
+    public boolean isComplete() {
+        return x > 0 && y > 0 && distTo > 0 && distNext > 0 && angleNext != null;
     }
+
 }
--- Coders-Strike-Back/src/test/java/PlayerTest.java
+++ Coders-Strike-Back/src/test/java/PlayerTest.java
@@ -12,18 +12,18 @@ import org.junit.Test;
     @Test
     public void calculAngle() {
         //A(2;1), B(−2;4) et C(1;−3)
-        Coord pA = new Coord(2, 1);
-        Coord pB = new Coord(-2, 4);
-        Coord pC = new Coord(1, -3);
+        Target pA = new Target(1, 2, 1);
+        Target pB = new Target(2,-2, 4);
+        Target pC = new Target(3,1, -3);
 
-        assertEquals(Double.valueOf(112.83), Player.calculAngle(pA, pB, pC), 0.1d);
+        assertEquals(112, Player.calculAngle(pA, pB, pC));
     }
 
     @Test
     public void calculDistance() {
         //A(2;1), B(−2;4)
-        Coord p1 = new Coord(2, 1);
-        Coord p2 = new Coord(-2, 4);
+        Target p1 = new Target(1, 2, 1);
+        Target p2 = new Target(2,-2, 4);
         
         assertEquals(Double.valueOf(5), Player.calculDistance(p1, p2));
     }
@@ -31,9 +31,9 @@ import org.junit.Test;
     @Test
     public void calculProduitScalaire() {
         //A(2;1), B(−2;4) et C(1;−3)
-        Coord pA = new Coord(2, 1);
-        Coord pB = new Coord(-2, 4);
-        Coord pC = new Coord(1, -3);
+        Target pA = new Target(1, 2, 1);
+        Target pB = new Target(2, -2, 4);
+        Target pC = new Target(3, 1, -3);
 
         assertEquals(Double.valueOf(-8d), Player.calculProduitScalaire(pA, pB, pC));
     }
